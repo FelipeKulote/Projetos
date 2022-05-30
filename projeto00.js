0ffb408dd1b02d269b2bbdb11d5b98ce72264b4d
FelipeKulote <lipe_augustobq@hotmail.com>
2022-05-30 19:43:24 -0300

    Verificando os ;

--- projeto00.js
+++ projeto00.js
@@ -1,30 +1,30 @@
 const prompt = require("prompt-sync")();
-console.clear()
+console.clear();
 
 //              ###    PROJETO DETETIVE    ###
 
-console.log(`Conferindo o caderno de anotações saberei se João é culpado ou não...`)
+console.log(`Conferindo o caderno de anotações saberei se João é culpado ou não...`);
 
 let x = 0
 
-let p1 = prompt('A arma do crime foi encontrada na casa de João? ')
+let p1 = prompt('A arma do crime foi encontrada na casa de João? ');
 if (p1.toLowerCase() == 'sim') {x++}
-let p2 = prompt('A digital do João está na arma do crime? ')
+let p2 = prompt('A digital do João está na arma do crime? ');
 if (p2.toLowerCase() == 'sim') {x++}
-let p3 = prompt('Alguma testemunha viu João na cena do crime? ')
+let p3 = prompt('Alguma testemunha viu João na cena do crime? ');
 if (p3.toLowerCase() == 'sim') {x++}
 let p4 = prompt('Outras pessoas já relataram que João tinha desavenças com a vítima? ')
 if (p4.toLowerCase() == 'sim') {x++}
-let p5 = prompt('João se beneficiaria com a morte da vítima? ')
+let p5 = prompt('João se beneficiaria com a morte da vítima? ');
 if (p5.toLowerCase() == 'sim') {x++}
 
 if (x >= 4) {
-    console.log('João é culpado!')
+    console.log('João é culpado!');
 }
 else if (x == 3) {
-    console.log('João é um suspeito!')
+    console.log('João é um suspeito!');
 }
 else {
-    console.log('João é inocente!')
+    console.log('João é inocente!');
 }
 
--- projeto01.js
+++ projeto01.js
@@ -1,5 +1,5 @@
 const prompt = require("prompt-sync")();
-console.clear()
+console.clear();
 
 //                    ###    DESAFIO DO HERÓI    ###
 // Criar uma história com 5 desafios a serem concluídos, cada desafio deverá ser respondido com "sim" ou "não"
@@ -19,7 +19,7 @@ while(p1.toLowerCase() != 'sim' && p1.toLowerCase() != 'nao'){
     console.log('Responda "sim" ou "nao"');
     p1 = prompt('Você procurou uma escola com bons índices de aprovação? ');
 }
-if (p1.toLowerCase() == 'sim') {x++};
+if (p1.toLowerCase() == 'sim') {x++}
 
 let p2 = prompt('Você está estudando para passar na prova teórica? ');
 while(p2.toLowerCase() != 'sim' && p2.toLowerCase() != 'nao'){
@@ -33,35 +33,35 @@ while(p3.toLowerCase() != 'sim' && p3.toLowerCase() != 'nao'){
     console.log('Responda "sim" ou "nao"');
     p3 = prompt('Você acertou mais de 25 questões da prova teórica? ');
 }
-if (p3.toLowerCase() == 'sim') {x++};
+if (p3.toLowerCase() == 'sim') {x++}
 
 let p4 = prompt('Você entendeu tudo sobre as aulas práticas de direção de veículos? ');
 while(p4.toLowerCase() != 'sim' && p4.toLowerCase() != 'nao'){
     console.log('Responda "sim" ou "nao"');
     p4 = prompt('Você entendeu tudo sobre as aulas práticas de direção de veículos? ');
 }
-if (p4.toLowerCase() == 'sim') {x++};
+if (p4.toLowerCase() == 'sim') {x++}
 
 let p5 = prompt('Você passou no exame de direção? ');
 while(p5.toLowerCase() != 'sim' && p5.toLowerCase() != 'nao'){
     console.log('Responda "sim" ou "nao"');
     p5 = prompt('Você passou no exame de direção? ');
 }
-if (p5.toLowerCase() == 'sim') {x++};
+if (p5.toLowerCase() == 'sim') {x++}
 console.log();
 
 if (x == 0) {
-    console.log('Que vergonha... Você não está nem tentando alcançar seu sonho!!')
+    console.log('Que vergonha... Você não está nem tentando alcançar seu sonho!!');
 }
 else if (x == 1 || x == 2) {
-    console.log('Se esforce, você é capaz de mais.')
+    console.log('Se esforce, você é capaz de mais.');
 }
 else if (x == 3) {
-    console.log('Você está quase lá!! Não desanime e tire todas as dúvidas com seu instrutor.')
+    console.log('Você está quase lá!! Não desanime e tire todas as dúvidas com seu instrutor.');
 }
 else if (x == 4) {
-    console.log('Essa bendita baliza, sempre causando problemas, mas treine mais, no próximo exame você com certeza conseguirá!!')
+    console.log('Essa bendita baliza, sempre causando problemas, mas treine mais, no próximo exame você com certeza conseguirá!!');
 }
 else {
-    console.log('QUE MARAVILHA, VOCÊ CONSEGUIU!! AGORA FINALMENTE PODERÁ REALIZAR SEU SONHO E CONHECER MUITOS LUGARES NOVOS!!')
-};
+    console.log('QUE MARAVILHA, VOCÊ CONSEGUIU!! AGORA FINALMENTE PODERÁ REALIZAR SEU SONHO E CONHECER MUITOS LUGARES NOVOS!!');
+}
