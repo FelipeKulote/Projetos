11c258a97863ba234666606e5095db087b28cc59
FelipeKulote <lipe_augustobq@hotmail.com>
2022-06-03 19:17:25 -0300

    Terminando o projeto 02

--- projeto02.js
+++ projeto02.js
@@ -7,11 +7,9 @@ const jokenpo = ['pedra', 'papel', 'tesoura'];
 let pontosJ = 0
 let pontosM = 0
 
-// let opcao = +prompt(`Escolha uma opção: 
-// 1: Jogar com um número de rodadas pré-estabelecido.
-// 2: Jogar por um número indeterminado de rodadas.`)
-
-// if (opcao == 1){
+let opcao = +prompt('Escolha uma opção: \n1: Escolher quantas rodadas eu quero jogar. \n2: Jogar sem uma quantidade certa de rodadas.')
+console.log();
+if (opcao == 1){
     let rodadas = +prompt('Quantas rodadas serão jogadas? ')
     for(let i = 0; i < rodadas; i++){
         console.log('Vamos à rodada ', i + 1,'!');
@@ -24,7 +22,7 @@ let pontosM = 0
         console.log();
         console.log(`Você escolheu ${jokenpo[escolha-1]}`);
         console.log();
-        let maquina  = Math.floor(Math.random() * 3)
+        let maquina  = Math.floor(Math.random() * 3) // A escolha para a máquina será aleatória
         console.log(`A máquina escolheu ${jokenpo[maquina]}`);   
         console.log();
 
@@ -69,16 +67,69 @@ let pontosM = 0
         você: ${pontosJ}
         Máquina: ${pontosM}`);
         console.log();
+        prompt('Digite algo para continuar: ')
     }
-// } else while (true){
-//     let x = 0
-//     console.log('Vamos a rodada ', x + 1,'!')
+} else {
+    while (true) {
+        let x = 0
+        console.log('Vamos à rodada ', x + 1,'!');
+        console.log();
+        console.log(`Você deve escolher uma opção, essa escolha será feita através de números:
+        1 - pedra
+        2 - papel
+        3 - tesoura.`); // Para que o jogador faça a escolha dele  
+        let escolha = +prompt('Escolha uma opção: ') // O número escolhido será usado como o índice do array
+        console.log();
+        console.log(`Você escolheu ${jokenpo[escolha-1]}`);
+        console.log();
+        let maquina  = Math.floor(Math.random() * 3) // A escolha para a máquina será aleatória
+        console.log(`A máquina escolheu ${jokenpo[maquina]}`);   
+        console.log();
 
-// }
-
-
-
-// let sair = prompt('Deseja continuar jogando? ');
-//     if(sair == 'nao'){
-//         break;
-//     }
+        if (jokenpo[escolha-1] == 'pedra'){
+            if (jokenpo[maquina] == 'pedra'){
+                console.log('Empate!');
+            }
+            else if (jokenpo[maquina] == 'tesoura'){
+                console.log('Você ganhou!')
+                pontosJ++
+            } else { 
+                console.log('Você perdeu!')
+                pontosM++ 
+            }
+        }
+        else if (jokenpo[escolha-1] == 'papel'){
+            if (jokenpo[maquina] == 'papel'){
+                console.log('Empate!');
+            }
+            else if (jokenpo[maquina] == 'pedra'){
+                console.log('Você ganhou!')
+                pontosJ++
+            } else {
+                console.log('Você perdeu!');
+                pontosM++
+            }
+        }
+        else {
+            if (jokenpo[maquina] == 'tesoura'){
+                console.log('Empate!');
+            }
+            else if (jokenpo[maquina] == 'papel'){
+                console.log('Você ganhou!');
+                pontosJ++
+            } else {
+                console.log('Você perdeu!')
+                pontosM++
+            }
+        }
+        console.log();
+        console.log(`O placar é de: 
+        você: ${pontosJ}
+        Máquina: ${pontosM}`);
+        console.log();
+        let  sair = prompt('Deseja continuar jogando? ')
+        if (sair == 'nao'){
+            break;
+        }
+    }
+}
